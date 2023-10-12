50adb30653977779ae48d745d016fbba9c36d46b
elliot-brooks <ellitothd@gmail.com>
2023-10-12 14:13:54 +0100

    Added boiler plate RotorTest

--- run_tests.bat
+++ run_tests.bat
@@ -2,4 +2,5 @@
 javac .\src\test\enigma\*.java  --source-path .\src\ -d .\bin\ --class-path .\lib\*
 java --enable-preview -cp .\bin\;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar org.junit.runner.JUnitCore^
  test.enigma.PlugboardTest^
- test.enigma.ReflectorTest
+ test.enigma.ReflectorTest^
+ test.enigma.RotorTest
---
+++ src/test/enigma/RotorTest.java
@@ -0,0 +1,12 @@
+package test.enigma;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RotorTest {
+    @Test
+    public void testEncryption() {
+        assertTrue(true);
+    }
+}
