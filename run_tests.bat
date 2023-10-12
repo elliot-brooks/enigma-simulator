950b8a882db60e156a807cf1be7302cd919ecd31
elliot-brooks <ellitothd@gmail.com>
2023-10-12 13:42:46 +0100

    Hotfix
    * Fixed error on run_test.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,5 @@
 @echo off
-javac .\src\test\enigma\*.java  --source-path .\src\ -d .\bin\ --class-path .\lib\*.jar
+javac .\src\test\enigma\*.java  --source-path .\src\ -d .\bin\ --class-path .\lib\*
 java --enable-preview -cp .\bin\;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar org.junit.runner.JUnitCore^
  test.enigma.PlugboardTest^
  test.enigma.ReflectorTest
