dcaa92c92ed07a0d6d93bedeedcf55363eea0835
elliot-brooks <ellitothd@gmail.com>
2023-10-12 16:13:08 +0100

    Enhanced Reflector Tests
    * Added more test cases for Reflector
    * Added a new tool
    
    Closes #9

--- src/main/tools/Tools.java
+++ src/main/tools/Tools.java
@@ -2,4 +2,8 @@ package main.tools;
 
 public class Tools {
 
+    public static char convertIndexToCharacter(int characterIndex) {
+        return (char) (characterIndex + Constants.JAVA_A_VALUE);
+    }
+
 }
