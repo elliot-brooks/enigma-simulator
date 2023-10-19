d773e54fbdb275cf81ba6f6dfee100a5f11c3208
elliot-brooks <ellitothd@gmail.com>
2023-10-19 08:45:54 +0100

    HotFix

--- src/main/cli/EnigmaSimulator.java
+++ src/main/cli/EnigmaSimulator.java
@@ -7,14 +7,15 @@ import main.enigma.Enigma;
 
 public class EnigmaSimulator {
     public static void main(String[] args) {
+        ComponentCache cache = new ComponentCache();
         try {
-            ComponentCache cache = new ComponentCache();
             cache.initialise();
         } catch (SAXException e) {
             e.printStackTrace();
         }
         String message = "H3LLO world!!";
         Enigma machine = Enigma.createDefaultEnigma();
+        machine.setRotor(0, cache.getRotor("MyCustomRotor"));
         System.out.println(machine.getCurrentSettings() + '\n');
         System.out.println(message + " = \n" + machine.encrypt(message));
     }
