706d47fd793604f2e39adb90b2facb3d7e980509
elliot-brooks <ellitothd@gmail.com>
2023-10-27 12:35:05 +0100

    Created Basic Placeholder app

--- src/main/app/TestApp.java
+++ src/main/app/TestApp.java
@@ -1,7 +1,23 @@
 package main.app;
 
-public class TestApp {
-public static void main(String args[]) {
-        return;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class TestApp extends Application{
+    @Override
+    public void start(Stage stage){
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
