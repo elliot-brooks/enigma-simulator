5f2a922235c411d8a53257773d18da8c9706c4b7
elliot-brooks <ellitothd@gmail.com>
2023-10-29 11:29:34 +0000

    Removed Unused imports

--- src/main/app/EnigmaMachineSimulatorApp.java
+++ src/main/app/EnigmaMachineSimulatorApp.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import main.enigma.Enigma;
 
 public class EnigmaMachineSimulatorApp extends Application{
     @Override
