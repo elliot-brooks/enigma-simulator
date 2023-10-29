696a482c2949f738e0ba0d3e220c98dace454e41
elliot-brooks <ellitothd@gmail.com>
2023-10-29 16:42:09 +0000

    Added MacOS Javafx
    * Moved build tools to specific folders
    * Added javafx lib for macos

--- run_tests.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-javac .\src\test\enigma\*.java  --source-path .\src\ -d .\bin\ --class-path .\lib\*
-java --enable-preview -cp .\bin\;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar org.junit.runner.JUnitCore^
- test.enigma.PlugboardTest^
- test.enigma.ReflectorTest^
- test.enigma.RotorTest^
- test.enigma.EnigmaTest
