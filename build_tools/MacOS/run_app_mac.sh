5e6d521ceb30dda4156f0648e659648d9cc22060
elliot-brooks <ellitothd@gmail.com>
2023-10-30 21:15:00 +0000

    Removed old build tools

--- build_tools/MacOS/run_app_mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find src/main/ -name "*.java" > sources.txt
-javac @sources.txt --source-path ./src/ -d ./bin/ --module-path ./lib/javafx-sdk-21.0.1-macos/lib --add-modules javafx.controls
-java -cp ./bin --module-path ./lib/javafx-sdk-21.0.1-macos/lib --add-modules javafx.controls main.app.EnigmaMachineSimulatorApp 
--- build_tools/Windows/compile_all.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-dir /s /B *.java > sources.txt
-javac @sources.txt --source-path .\src\ -d .\bin\ --class-path .\lib\* --module-path .\lib\openjfx-21.0.1_windows-x64_bin-sdk\javafx-sdk-21.0.1\lib --add-modules javafx.controls
